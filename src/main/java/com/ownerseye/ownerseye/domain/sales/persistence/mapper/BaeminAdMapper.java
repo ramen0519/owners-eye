64d0922c1c6762f5e729bf1a9a8447b25bf48785
ramen0519 <ouroboros0519@gmail.com>
2026-05-12 04:06:49 +0900

    feat: 매출 데이터 조회 Mapper, XML 추가

--- src/main/java/com/ownerseye/ownerseye/domain/sales/persistence/mapper/BaeminAdMapper.java
+++ src/main/java/com/ownerseye/ownerseye/domain/sales/persistence/mapper/BaeminAdMapper.java
@@ -2,9 +2,16 @@ package com.ownerseye.ownerseye.domain.sales.persistence.mapper;
 
 import com.ownerseye.ownerseye.domain.sales.persistence.entity.BaeminAdEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Mapper
 public interface BaeminAdMapper {
 
     void save(BaeminAdEntity baeminAd);
+
+    Optional<BaeminAdEntity> findByStoreIdAndYearMonth(@Param("storeId") Long storeId,
+                                                       @Param("yearMonth") LocalDate yearMonth);
 }
